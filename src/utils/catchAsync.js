9adff06d681c64c291ba5edb0525b328287e1209
Shakhlyn <shakhlyn.sh.du@gmail.com>
2023-12-29 00:20:30 +0600

    feat: Implement reusable asynchronous error handler function with try catch block

--- src/utils/catchAsync.js
+++ src/utils/catchAsync.js
@@ -1,22 +1,9 @@
-class ApiError extends Error {
-  constructor(
-    statusCode,
-    message = "Something went wrong!",
-    errors = [],
-    stack = ""
-  ) {
-    super(message);
-    this.statusCode = statusCode;
-    this.errors = errors;
-    this.data = null;
-    this.success = false;
-
-    if (stack) {
-      this.stack = stack;
-    } else {
-      Error.captureStackTrace(this, this.constructor);
-    }
+const catchAsync = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    next(error);
   }
-}
+};
 
-export default ApiError;
+export default catchAsync;
